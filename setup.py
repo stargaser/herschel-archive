9deaa5d4711bdbc7fbfcc1e1c7eba0269c054d07
David Shupe <dave.shupe@gmail.com>
2016-03-18 11:04:40 -0700

    1.0.1dev version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='hsadownload',
-    version='1.0.0.dev',
+    version='1.0.1.dev',
     description='Herschel Archive Download Utilities',
     author='David Shupe',
     author_email='shupe@ipac.caltech.edu',
