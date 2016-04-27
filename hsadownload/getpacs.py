675b27dc0b2a2521a13405f127dc7680407ea166
David Shupe <dave.shupe@gmail.com>
2016-04-27 16:15:27 -0700

    pass spgVersion to functions

--- hsadownload/getpacs.py
+++ hsadownload/getpacs.py
@@ -14,7 +14,7 @@ def downloadPacsMap(ldict, obsid, lev, bandKey, direc='./PacsPhoto/', \
     """
     Download PACS map. Not meant to be called directly but to be called by
     storePacsPhoto.
-    
+
     Parameters:
     -----------
     ldict (dict) : dictionary of level labels and urn strings
@@ -37,11 +37,11 @@ def downloadPacsMap(ldict, obsid, lev, bandKey, direc='./PacsPhoto/', \
             print('downloaded ' + filename)
     else:
         print('did not find %s in %s for %s' %(bandKey, lev, obsid))
-        
+
 def storePacsPhoto(obsid, spgVersion='SPG v13.0.0', direc='./PacsPhoto/', blueCheck=True):
     """
     Download and store a PACS map
-    
+
     Parameters:
     -----------
     obsid (long int): observation id
@@ -50,41 +50,45 @@ def storePacsPhoto(obsid, spgVersion='SPG v13.0.0', direc='./PacsPhoto/', blueCh
     blueCheck (boolean) : Check whether to download the blue file even if it does
       not match the obsid001 metadata. Defaults to True (check). Set to False to
       force the download of the blue file
-      
+
     Returns:
     --------
     Returns 0 if no no level3, level2_5 or level2 in observation; otherwise 1
     """
     instrument = 'PACS'
-    urn = getObsUrn(obsid,instrument)
+    urn = getObsUrn(obsid,instrument,spgVersion=spgVersion)
     hdulist = getHsaFits(urn)
     cdict = parseContextHdu(hdulist)
-    if 'level3' in cdict: 
+    if 'level3' in cdict:
         lev = 'L3'
         bandKey = 'HPPJSMAPR'
         lhdulist = getHsaFits(cdict['level3'])
         ldict = parseContextHdu(lhdulist)
         if (bandKey in ldict):
             if (obsid == lhdulist[0].header['obsid001']):
-                downloadPacsMap(ldict, obsid, lev, bandKey, direc)
+                downloadPacsMap(ldict, obsid, lev, bandKey, direc,
+                                  spgVersion=spgVersion)
             else:
                 print('skipping %s for %s since obsid001 is %s' % (bandKey, obsid, lhdulist[0].header['obsid001']))
         bandKey = 'HPPJSMAPB'
         if (bandKey in ldict):
             if ((blueCheck == False) or (obsid == lhdulist[0].header['obsid001'])):
-                downloadPacsMap(ldict, obsid, lev, bandKey, direc)
+                downloadPacsMap(ldict, obsid, lev, bandKey, direc,
+                                     spgVersion=spgVersion)
         elif ('HPPJSMAPG' in ldict):
             if ((blueCheck == False) or (obsid == lhdulist[0].header['obsid001'])):
-                downloadPacsMap(ldict, obsid, lev, 'HPPJSMAPG', direc)
+                downloadPacsMap(ldict, obsid, lev, 'HPPJSMAPG', direc,
+                                   spgVersion=spgVersion)
         else: # get Level 2.5
             lev = 'L25'
             lhdulist = getHsaFits(cdict['level2_5'])
             ldict = parseContextHdu(lhdulist)
             if ((blueCheck == False) or (obsid == lhdulist[0].header['obsid001'])):
-                downloadPacsMap(ldict, obsid, lev, bandKey, direc)
+                downloadPacsMap(ldict, obsid, lev, bandKey, direc,
+                                   spgVersion=spgVersion)
             else:
                 print('skipping %s for %s since obsid001 is %s' % (bandKey, obsid, lhdulist[0].header['obsid001']))
-                
+
     elif 'level2_5' in cdict:
         lev = 'L25'
         lhdulist = getHsaFits(cdict['level2_5'])
@@ -92,7 +96,8 @@ def storePacsPhoto(obsid, spgVersion='SPG v13.0.0', direc='./PacsPhoto/', blueCh
         for bandKey in ['HPPJSMAPR','HPPJSMAPB']:
             if (bandKey in ldict):
                 if (((blueCheck == False) and (bandKey[-1] == 'B')) or (obsid == lhdulist[0].header['obsid001'])):
-                    downloadPacsMap(ldict, obsid, lev, bandKey, direc)
+                    downloadPacsMap(ldict, obsid, lev, bandKey, direc,
+                                     spgVersion=spgVersion)
                 else:
                     print('skipping %s for %s since obsid001 is %s' % (bandKey, obsid, lhdulist[0].header['obsid001']))
     elif 'level2' in cdict:
@@ -100,20 +105,21 @@ def storePacsPhoto(obsid, spgVersion='SPG v13.0.0', direc='./PacsPhoto/', blueCh
         lhdulist = getHsaFits(cdict['level2'])
         ldict = parseContextHdu(lhdulist)
         for bandKey in ['HPPPMAPR','HPPPMAPB']:
-            downloadPacsMap(ldict, obsid, lev, bandKey, direc)
+            downloadPacsMap(ldict, obsid, lev, bandKey, direc,
+                               spgVersion=spgVersion)
     else:
         return(0)
     return(1)
-    
+
 def imageContents(fname):
     """
     Return a sequence of (obsid, fname, band) tuples contained in a PACS image.
     Intended for use with Spark's flatMap transformatios.
-    
+
     Parameters:
     -----------
     fname: string containing filename for PACS Level 2, Level 2.5 or Level 3 image
-    
+
     Returns:
     --------
     reslist: Python list of (obsid, fname, band) values
