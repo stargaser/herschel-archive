70f460d3d0ba4c605704bb7179e3618583311621
David Shupe <dave.shupe@gmail.com>
2016-05-11 09:58:46 -0700

    add docstrings for SPIRE funcs

--- hsadownload/getspire.py
+++ hsadownload/getspire.py
@@ -11,6 +11,20 @@ import astropy.io.fits as fits
 
 def downloadSpireMap(ldict, obsid, lev, bandKey, direc='./SpirePhoto/', \
     spgVersion='SPG v13.0.0', overWrite=False):
+    """
+    Download SPIRE map. Not meant to be called directly but to be called by
+    storeSpirePhoto.
+
+    Parameters:
+    -----------
+    ldict (dict) : dictionary of level labels and urn strings
+    obsid (long int) : observation id
+    lev (string) : level being downloaded
+    bandKey (string) : key for band being downloaded
+    direc (string) : directory in which to store file, defaults to './SpirePhoto/'
+    spgVersion (string) : pipeline version, to put in output filename
+    overwrite (bool) : overwrite file if it already exists? defaults to False
+    """
     normVersion = ''.join(spgVersion.split())
     if bandKey in ldict:
         filename = os.path.join(direc,"%s_SPIRE_%s_%s_%s.fits.gz"%(obsid,lev,bandKey,normVersion))
@@ -21,6 +35,18 @@ def downloadSpireMap(ldict, obsid, lev, bandKey, direc='./SpirePhoto/', \
         print('did not find %s in %s for %s' %(bandKey, lev, obsid))
 
 def storeSpirePhoto(obsid, spgVersion='SPG v13.0.0', direc='./SpirePhotoScan/'):
+        """
+    Download and store a SPIRE map
+
+    Parameters:
+    -----------
+    obsid (long int): observation id
+    spgVersion (string) : pipeline version, defaults to 'SPG v13.0.0'
+    direc (string) : path to directory for storing files, defaults to './SpirePhoto/'
+    Returns:
+    --------
+    Returns 0 if no level2_5 or level2 in observation; otherwise 1
+    """
     instrument = 'SPIRE'
     normVersion = ''.join(spgVersion.split())
     urn = getObsUrn(obsid,instrument,spgVersion=spgVersion)
